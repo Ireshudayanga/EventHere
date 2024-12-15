c6172782ae20245b47d71d63c7fccf8ddbe455a5
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2024-12-15 21:32:36 +0530

    update Footer component text; enhance Navbar with new menu items and styling; add Vite SVG file; create VSCode settings file

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,15 +5,15 @@ const Navbar = () => {
   return (
     <div className=' flex flex-row items-center justify-between'>
       <div className='w-52'><img src={logo} alt="EventHere Logo" /></div>
-      <div className='w-[400px]'>
+      <div className='w-[400px] bg-[#2c302e1d]'>
         <ul
           tabIndex={0}
-          className="flex flex-row justify-between">
-          <li><a>Item 1</a></li>
-          <li><a>Parent</a></li>
-          <li><a>Item 3</a></li>
-          <li><a>Item 3</a></li>
-          <li><a>Item 3</a></li>
+          className="flex flex-row items-center m-4 justify-between h-14">
+          <li><a>Home</a></li>
+          <li><a>Events</a></li>
+          <li><a>Calendar</a></li>
+          <li><a>Volunteer</a></li>
+          <li><a>Explore</a></li>
         </ul>
       </div>
       <div className=''>login</div>
