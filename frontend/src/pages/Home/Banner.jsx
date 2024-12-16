1d3472e3cf85ee8ac537189892456e2445e4fb24
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2024-12-16 21:41:25 +0530

    update Navbar and Banner components; modify button styles for improved visibility; add new image frame styles in Home CSS

--- frontend/src/pages/Home/Banner.jsx
+++ frontend/src/pages/Home/Banner.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>
-      Banner
+    <div className=''>
+      <div className='w-1/2 flex flex-row relative pt-[200px]'>
+      <div className='imgframe1 absolute'></div>
+      <div className='imgframe2 absolute top-[350px] left-[140px]'></div>
+      <div className='imgframe3 absolute left-[350px]'></div>
+      </div>
+      <div className='w-1/2'>
+      </div>
     </div>
   )
 }
