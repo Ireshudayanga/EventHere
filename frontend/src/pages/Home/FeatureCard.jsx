2be3644e2b69402a0c0a511d5f1805abc0fa232a
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2024-12-22 12:20:46 +0530

    add FeatureCard component; implement layout for feature display and update styles for improved aesthetics

---
+++ frontend/src/pages/Home/FeatureCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const FeatureCard = () => {
+    return (
+        <div className="my-44 flex  flex-col items-center">
+            <div className="flex flex-row gap-16 my-10 justify-center">
+                <div className="featureframe ">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="featureframe ">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="featureframe ">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div className="flex flex-row gap-16 my-10 justify-center">
+                <div className="featureframe ">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="featureframe ">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="featureframe ">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeatureCard
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import '../Home/Home.css'
 import Hero from './Hero'
 import Banner from './Banner'
+import FeatureCard from './FeatureCard'
 const Home = () => {
   return (
     <div>
-      <div className=' my-16 lg:my-28 px-6 md:px-40'><Hero /></div>
+      <div className=' my-16 lg:my-28 px-6 md:px-40'>
+        <Hero />
+      </div>
       <div className=" px-2 md:px-6">
         <Banner />
+      </div>
+      <div className="px-2 md:px-6">
+        <FeatureCard/>
       </div>
     </div>
   )
