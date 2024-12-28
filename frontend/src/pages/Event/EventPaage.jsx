de0a19f66bd8be2b28b339d2aeae342032c5f09e
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2024-12-28 15:40:16 +0530

    update EventPage component; enhance layout with improved padding and shadow effects for better visual hierarchy

--- frontend/src/pages/Event/EventPaage.jsx
+++ frontend/src/pages/Event/EventPaage.jsx
@@ -18,25 +18,24 @@ const EventPage = () => {
             </div>
 
             {/* Main Content */}
-            <div className="bg-[#e3dddd] h-[92%] w-[94%]  mt-4 rounded-2xl shadow-md ">
+            <div className="bg-[#d4d3d3] h-[92%] w-[94%] mt-4 rounded-2xl shadow-md ml-auto">
                 {/* Main Layout */}
-                <div className="flex  h-full p-3">
+                <div className="flex h-full p-7">
 
                     {/* Left Section */}
                     <div className="w-[25%] h-full flex flex-col gap-5">
-                        <div className="bg-white text-black p-5 rounded-2xl h-[35%]">
+                        <div className="bg-white text-black p-5 rounded-2xl h-[35%] shadow-lg">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptates.
                         </div>
-                        <div className="bg-white text-black p-5 rounded-2xl  h-[65%]">
+                        <div className="bg-white text-black p-5 rounded-2xl h-[65%] shadow-lg">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptates.
                         </div>
                     </div>
 
-
                     {/* Right Section */}
-                    <div className="  w-[75%] h-full  text-zinc-950 flex flex-col pl-3">
+                    <div className="w-[75%] h-full text-zinc-950 flex flex-col pl-6">
                         <div className="h-2/3 overflow-y-auto">
-                            <div className="bg-white text-black p-5 rounded-2xl  h-[100%]">
+                            <div className="bg-white text-black p-5 rounded-2xl h-[100%] shadow-lg">
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptates.
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. A, iusto harum. Quaerat ad accusamus nemo quis nihil aliquid deserunt voluptatum! Minus ut harum quasi aperiam atque laudantium optio aut cumque.
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eius labore non odit? Iusto laborum fugiat deserunt qui obcaecati reiciendis doloremque quisquam dolores quia nemo perferendis quod, assumenda inventore possimus?
@@ -44,21 +43,22 @@ const EventPage = () => {
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ab. Vel magni repellendus voluptatem sed, ipsa deleniti provident eos numquam? Maxime nesciunt iure facilis est fugiat natus nam praesentium cum.
                             </div>
                         </div>
-                        <div className="flex gap-3 justify-between pt-3 h-1/3">
-                            <div className="bg-white text-black rounded-2xl w-1/3 h-[100%] p-4 ">
+                        <div className="flex gap-3 justify-between pt-5 h-1/3">
+                            <div className="bg-white text-black rounded-2xl w-1/3 h-[100%] p-4 shadow-lg">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime.
                                 lorem lorem
                             </div>
-                            <div className="bg-white text-black rounded-2xl w-1/3 h-[100%] p-4 ">
+                            <div className="bg-white text-black rounded-2xl w-1/3 h-[100%] p-4 shadow-lg">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime.
                             </div>
-                            <div className="bg-white text-black rounded-2xl w-1/3 h-[100%] p-4 ">
+                            <div className="bg-white text-black rounded-2xl w-1/3 h-[100%] p-4 shadow-lg">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam maxime.
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
