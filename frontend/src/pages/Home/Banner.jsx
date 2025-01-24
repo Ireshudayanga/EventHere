fe6c66cde5c6ba712f31e472014add29f78d473a
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2025-01-24 12:02:33 +0530

    refactor Banner component; enhance image layout and responsiveness for better visual alignment

--- frontend/src/pages/Home/Banner.jsx
+++ frontend/src/pages/Home/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import feature01 from '../../assets/svg/feature01.svg'; 
+import feature01 from '../../assets/svg/feature01.svg';
 import feature02 from '../../assets/svg/feature02.svg';
 import feature03 from '../../assets/svg/feature03.svg';
 
@@ -7,22 +7,21 @@ const Banner = () => {
   return (
     <div className=" flex flex-row  w-full  mt-44 ">
       {/* Left side - Images */}
-      {/* Left side - Images */}
+
       <div className="relative flex flex-col md:flex-row md:w-1/2">
-        
         {/* Frame 1 */}
-        <div className="imgframe1  md:top-[50px] md:left-0">
-          <img className=" md:w-[180px] transform scale-x-[-1] mt-[50px]" src={feature01} alt="feature01" />
+        <div className="imgframe1 flex justify-center items-center md:top-[50px] md:left-0">
+          <img className="w-[90px] md:w-[180px] transform scale-x-[-1] mt-[20px]" src={feature01} alt="feature01" />
         </div>
 
         {/* Frame 2 */}
-        <div className="imgframe2 m-auto md:absolute md:top-[160px] md:left-[120px]">
-          <img className="w-[120px] md:w-[200px] mt-4 md:mt-[100px]" src={feature02} alt="feature02" />
+        <div className="imgframe2 flex justify-center items-center m-auto md:absolute md:top-[160px] md:left-[120px]">
+          <img className="w-[120px] md:w-[200px] mt-4 md:mt-[60px]" src={feature02} alt="feature02" />
         </div>
 
         {/* Frame 3 */}
-        <div className="imgframe3 m-auto md:absolute md:left-[260px]">
-          <img className="w-[120px] md:w-[180px] ml-6 my-4 md:mt-[80px]" src={feature03} alt="feature03" />
+        <div className="imgframe3 flex justify-center items-center m-auto md:absolute md:left-[260px]">
+          <img className="w-[90px] md:w-[180px] md:ml-6 my-4 md:mt-[50px]" src={feature03} alt="feature03" />
         </div>
       </div>
 
@@ -30,8 +29,8 @@ const Banner = () => {
       <div className="md:w-1/2 text-center md:text-left">
         <h2 className="md:text-4xl text-3xl font-semibold mb-4 leading-tight">Power Up Your <br /> Events <br />With <span className='primary-color'>Event</span><span className='yellow-color'>Here</span></h2>
         <p className="my-10 md:my-14 text-gray-700 leading-loose">
-        EventHere makes finding, attending, and sharing events effortless. Discover events tailored to you, carpool with others, stay organized with a personal calendar, and connect through comments and reviews. Make an impact by volunteering or donating, 
-        and earn eco-friendly badges along the way. EventHere brings people together.
+          EventHere makes finding, attending, and sharing events effortless. Discover events tailored to you, carpool with others, stay organized with a personal calendar, and connect through comments and reviews. Make an impact by volunteering or donating,
+          and earn eco-friendly badges along the way. EventHere brings people together.
         </p>
       </div>
     </div>
