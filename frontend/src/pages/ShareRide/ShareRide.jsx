f8571043916e2f27a05127a988becd6cf3d5498a
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2025-02-09 21:26:46 +0530

    feat: refactor ShareRide component to include RideCard for displaying available rides

--- frontend/src/pages/ShareRide/ShareRide.jsx
+++ frontend/src/pages/ShareRide/ShareRide.jsx
@@ -3,14 +3,15 @@ import SearchBar from '../../components/SearchBar';
 import Button from '../../components/Button';
 import Map from '../../components/Map';
 import animationGif from '../../assets/animation/animation.gif';
-import userIcon from '../../assets/svg/User.svg';
 import { FaStar } from "react-icons/fa";
 import "../ShareRide/ShareRide.css";
+import RideCard from './RideCard';
 
 const ShareRide = () => {
 
   const [location1, setLocation1] = useState('');
   const [location2, setLocation2] = useState('');
+  const [availableRides, setAvailableRides] = useState(false); 
 
   return (
     <div className="h-screen w-full">
@@ -92,31 +93,16 @@ const ShareRide = () => {
 
                 {/* Left - Animation GIF */}
                 <div className=" flex items-center justify-center">
-                  <img src={animationGif} alt="animation" className="w-[300px] h-full " />
+                  <img src={animationGif} alt="animation" className="w-[300px] md:w-[150px] h-full " />
                 </div>
 
                 {/* Right - User Request Card */} 
-                <div className="bg-gray-100 rounded-2xl shadow-md p-4 flex flex-col w-full items-center justify-center">
-                  <div className="flex items-center gap-4 mb-2">
-                    <img src={userIcon} alt="user" className="w-10 h-10 object-contain" />
-                    <div>
-                      <p className="text-lg font-medium text-gray-800">Michael Smith</p>
-                      <div className="flex items-center gap-1 text-yellow-500">
-                        <FaStar />
-                        <span className="text-sm font-medium text-gray-700">4.3</span>
-                      </div>
-                    </div>
-                  </div>
-                  <p className="text-sm text-gray-600 mb-4">Request Ride With You?</p>
-                  <div className="flex gap-3">
-                    <Button className="bg-green-500 text-white px-4 py-2 rounded-lg text-sm font-medium shadow-md hover:bg-gray-400">
-                      Accept
-                    </Button>
-                    <Button className="bg-gray-500 text-gray-700 px-4 py-2 rounded-lg text-sm font-medium shadow-md hover:bg-gray-400">
-                      Cancel
-                    </Button>
-                  </div>
-                </div>
+                {availableRides ? (
+                  <RideCard />
+                ) : ( <div className=" p-4  text-center">
+                  <p className="text-gray-400">No rides available at the moment</p>
+                </div>)}
+
               </div>
             </div>
           </div>
