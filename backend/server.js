cc354d8a7c3ed110d5b868134d06eaeeab387ddf
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2025-02-16 19:20:37 +0530

    feat: update backend dependencies, integrate MongoDB connection, and remove WebSocket server implementation

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,30 @@
 
 const express = require('express');
 const app = express();
-const webSocket = require('ws');
-const wss = new webSocket.Server({port: 8080});
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
 
 // Middleware (Optional)
 app.use(express.json()); 
 
-
-
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello, Docker !');
+    res.send('Hello, Docker hii !');
 });
 
+// Connect to MongoDB
+mongoose
+.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@eventhere-cluster.9jdmr.mongodb.net/?retryWrites=true&w=majority&appName=EventHere-Cluster`)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.log('Error:', error.message);
+}); 
 // Start the Server
 const PORT = 5000; // Choose your port number
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// WebSocket Server
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-
-    ws.on('message', (message) => {
-        console.log(`Received: ${message}`);
-
-        // Broadcast the message to all connected clients
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
-
-    ws.on('close', () => console.log('Client disconnected'));
-});
-
-console.log('WebSocket server running on ws://localhost:8080');
