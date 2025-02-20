efcda5d44dded1055e5549cb2e586b967418ea22
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2025-02-20 12:00:59 +0530

    chore: disable exhaustive-deps eslint rule in Map component

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents, LayersControl, useMap } from "react-leaflet";
