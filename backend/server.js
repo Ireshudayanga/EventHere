4880078e3e3ef10a40d05170c359b97ab39736e5
Iresh Udayanga <123167032+Ireshudayanga@users.noreply.github.com>
2025-03-19 11:53:30 +0530

    feat: add firebase-admin dependency and configure Firebase service account in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,21 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const http = require('http');
+const jwt = require("jsonwebtoken");
+
 require('dotenv').config();
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Fix newlines
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URI,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URI,
+  };
 
 // Middleware
 app.use(express.json());
